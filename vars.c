7564cbe49112530c39f5e65fb3abc67e36ecfc7c
MohamedAitHamoucha <aithamoucha.mohamed@gmail.com>
2023-05-21 11:11:47 -0700

    Mohamed Ait Hamoucha

--- vars.c
+++ vars.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * is_chain - test if current char in buffer is a chain delimeter
  * @info: the parameter struct
@@ -11,7 +10,6 @@
 int is_chain(info_t *info, char *buf, size_t *p)
 {
 	size_t j = *p;
-
 	if (buf[j] == '|' && buf[j + 1] == '|')
 	{
 		buf[j] = 0;
@@ -34,7 +32,6 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	*p = j;
 	return (1);
 }
-
 /**
  * check_chain - checks we should continue chaining based on last status
  * @info: the parameter struct
@@ -48,7 +45,6 @@ int is_chain(info_t *info, char *buf, size_t *p)
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
-
 	if (info->cmd_buf_type == CMD_AND)
 	{
 		if (info->status)
@@ -65,10 +61,8 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 			j = len;
 		}
 	}
-
 	*p = j;
 }
-
 /**
  * replace_alias - replaces an aliases in the tokenized string
  * @info: the parameter struct
@@ -80,7 +74,6 @@ int replace_alias(info_t *info)
 	int i;
 	list_t *node;
 	char *p;
-
 	for (i = 0; i < 10; i++)
 	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
@@ -107,7 +100,6 @@ int replace_vars(info_t *info)
 {
 	int i = 0;
 	list_t *node;
-
 	for (i = 0; info->argv[i]; i++)
 	{
 		if (info->argv[i][0] != '$' || !info->argv[i][1])
@@ -134,11 +126,7 @@ int replace_vars(info_t *info)
 		}
 
 		replace_string(&info->argv[i], _strdup(""));
-<<<<<<< HEAD
         }
-=======
-	}
->>>>>>> db8d0893a72e6ec0525231609e5b414fdce01953
 	return (0);
 }
 /**
